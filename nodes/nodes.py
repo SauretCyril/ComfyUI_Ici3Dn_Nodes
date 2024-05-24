5b45e011f2839985db76e63693d66870cb00b1f0
Cyril SAURET <sauretcyril@outlook.fr>
2024-05-24 22:05:09 +0200

    other modified

--- nodes/nodes.py
+++ nodes/nodes.py
@@ -18,11 +18,12 @@ class Ici3Dn_identity:
     #RETURN_TYPES = ("MASK","FLOAT","STRING")
     #RETURN_NAMES = ("MASK","New pos","Debug")
     FUNCTION = "Ici3Dn_identity"
-    OUTPUT_NODE = False
+    OUTPUT_NODE = True
     CATEGORY = "Ici3Dn_ComFyIU"
 		
     @classmethod
-    def Ici3Dn_identity(self,Original_workflow, ID):	
+    def Ici3Dn_identity(self,Original_workflow, ID):
+        	
         self.Original_workflow =Original_workflow
         self.ID
 		

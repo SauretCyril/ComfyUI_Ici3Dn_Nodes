3b4951653c2690c78c08b6efae8557758f53fdf1
Cyril SAURET <sauretcyril@outlook.fr>
2024-05-24 21:47:23 +0200

    Forgotenn I for i

--- __init__.py
+++ __init__.py
@@ -4,6 +4,6 @@ from .nodes.nodes import *
 
 NODE_CLASS_MAPPINGS = { 
 	"Ici3Dn Build Mask":Ici3Dn_Mask,
-    "ici3Dn identity":ici3Dn_identity}
+    "Ici3Dn identity":Ici3Dn_identity}
     
 print("\033[ComfyUI_Ici3Dn_Nodes: \033[92mLoaded\033[0m")

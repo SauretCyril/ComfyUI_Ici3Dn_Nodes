6d7b6ce8bd39ac69e729a970de0050e66d2822c2
Cyril SAURET <sauretcyril@outlook.fr>
2024-05-25 21:48:26 +0200

    update

--- nodes/nodes.py
+++ nodes/nodes.py
@@ -99,14 +99,18 @@ class Ici3Dn_Identity:
     
 class Ici3Dn_ShowText:
     @classmethod
-    def INPUT_TYPES(cls):
+    def INPUT_TYPES(s):
         return {
             "required": {
                 "text": ("STRING", {"forceInput": True}),
             },
+            "hidden": {
+                "unique_id": "UNIQUE_ID",
+                "extra_pnginfo": "EXTRA_PNGINFO",
+            },
         }
-   
-    #INPUT_IS_LIST = True
+
+    INPUT_IS_LIST = True
     RETURN_TYPES = ("STRING",)
     FUNCTION = "notify"
     OUTPUT_NODE = True
@@ -114,8 +118,23 @@ class Ici3Dn_ShowText:
 
     CATEGORY = "utils"
 
-    def notify(self, text):
-        result=text
-        
-        return {"ui": {"text": text}, "result": result}
+    def notify(self, text, unique_id=None, extra_pnginfo=None):
+        if unique_id is not None and extra_pnginfo is not None:
+            if not isinstance(extra_pnginfo, list):
+                print("Error: extra_pnginfo is not a list")
+            elif (
+                not isinstance(extra_pnginfo[0], dict)
+                or "workflow" not in extra_pnginfo[0]
+            ):
+                print("Error: extra_pnginfo[0] is not a dict or missing 'workflow' key")
+            else:
+                workflow = extra_pnginfo[0]["workflow"]
+                node = next(
+                    (x for x in workflow["nodes"] if str(x["id"]) == str(unique_id[0])),
+                    None,
+                )
+                if node:
+                    node["widgets_values"] = [text]
+
+        return {"ui": {"text": text}, "result": (text,)}
  

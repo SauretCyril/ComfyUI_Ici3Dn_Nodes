695b0f03f913deb272896860fe097bd6945342eb
Cyril SAURET <sauretcyril@outlook.fr>
2024-05-25 10:27:50 +0200

    move identity node

--- nodes/nodes.py
+++ nodes/nodes.py
@@ -60,3 +60,24 @@ class Ici3Dn_Mask:
 			
 			return (FinalMask,pxWidth,NewPosi,debugText)        
 
+class Ici3Dn_identity:
+    @classmethod
+    def INPUT_TYPES(cls):
+            return {"required": {					
+					"Original_workflow": ("STRING",),
+                    "ID": ("STRING",),
+                    }
+            }
+   
+    FUNCTION = "Ici3Dn_identity"
+    OUTPUT_NODE = True
+    CATEGORY = "Ici3Dn_ComFyIU"
+    RETURN_TYPES = ("STRING","STRING")
+    RETURN_NAMES = ("Original_workflow","ID")
+    @classmethod
+    def Ici3Dn_identity(self,Original_workflow, ID):
+        	
+        self.Original_workflow =Original_workflow
+        self.ID=ID
+        return (Original_workflow,ID)
+        

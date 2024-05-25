e6eb5bfe780c944fda4811b977ab78b4e0bb783c
Cyril SAURET <sauretcyril@outlook.fr>
2024-05-25 09:43:30 +0200

    test

--- __init__.py
+++ __init__.py
@@ -1,14 +1,13 @@
 from .nodes.nodes import *
 from .nodes.nodes_id import *
-from .nodes.nodes_test import *
+
 
 #from .nodes.loopback import *
 
 
 NODE_CLASS_MAPPINGS = { 
 	"Ici3Dn Build Mask":Ici3Dn_Mask,
-    "Ici3Dn identity":Ici3Dn_identity,
-    "Ici3Dn test":Ici3Dn_test}
+    "Ici3Dn identity":Ici3Dn_identity}
 
     
     

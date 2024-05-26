c97cfb5bc1f295986dca2419e350ec448ae7d368
Cyril SAURET <sauretcyril@outlook.fr>
2024-05-26 10:06:13 +0200

    up

--- web/js/showText.js
+++ web/js/showText.js
@@ -3,9 +3,9 @@ import { ComfyWidgets } from "../../../scripts/widgets.js";
 
 // Displays input text on a node
 app.registerExtension({
-	name: "ShowText25",
-	async beforeRegisterNodeDef(nodeType, nodeData, app) {
-		if (nodeData.name === "ShowText25") {
+	name: "Ici3Dn_Identity", async beforeRegisterNodeDef(nodeType, nodeData, app) 
+	{
+		if (nodeData.name === "Ici3Dn_Identity") {
 			function populate(text) {
 				if (this.widgets) {
 					for (let i = 1; i < this.widgets.length; i++) {
